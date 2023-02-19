96307985ca06a3c7f9b726ed1f030768b998f7e1
taisun93 <taisun93@gmail.com>
2023-02-18 19:39:58 -0600

    check

--- shell/wish.c
+++ shell/wish.c
@@ -1,1 +1,35 @@
-//Add your code here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+    char input[1000];
+    char *argv[100];
+    while (1) {
+        printf("shell> ");
+        fgets(input, 1000, stdin);
+        //parse the input command and split it into an array of arguments
+        int argc = 0;
+        char *token = strtok(input, " \n");
+        while (token != NULL) {
+            argv[argc++] = token;
+            token = strtok(NULL, " \n");
+        }
+        argv[argc] = NULL;
+        //create a child process to execute the command
+        pid_t pid = fork();
+        if (pid == 0) {
+            //child process
+            execvp(argv[0], argv);
+            //if execvp fails, print an error message
+            printf("%s: command not found\n", argv[0]);
+            exit(1);
+        } else {
+            //parent process
+            wait(NULL);
+        }
+    }
+    return 0;
+}

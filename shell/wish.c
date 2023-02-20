dfa642acab96040c15e411b91bf166609f89dd58
taisun93 <taisun93@gmail.com>
2023-02-19 22:34:04 -0600

    backwards

--- shell/wish.c
+++ shell/wish.c
@@ -210,16 +210,16 @@ int main(int argc, char *argv[])
             }
 
             // can't end on >
-            if (strcmp(args[i-1], ">") != 0)
+            if (strcmp(args[i-1], ">") == 0)
             {
                 fprintf(stderr, "An error has occurred\n");
                 fucking_bother = 0;
                 break;
             }
-            else{
-                fprintf(stderr, "kill me %s\n", args[i-1]);
+            // else{
+            //     fprintf(stderr, "kill me %s\n", args[i-1]);
                 
-            }
+            // }
 
             // Check for shell redirection
             int fd = -1;

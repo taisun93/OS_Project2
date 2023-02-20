6977b512f2569e3942a39c1384a721eae397d910
taisun93 <taisun93@gmail.com>
2023-02-20 14:06:18 -0600

    no log

--- shell/wish.c
+++ shell/wish.c
@@ -187,7 +187,7 @@ int main(int argc, char *argv[])
 
             for (i = 0; args[i] != NULL; i++)
             {
-                fprintf(stdout, "start start %s \n", args[i]);
+                // fprintf(stdout, "start start %s \n", args[i]);
                 if (strcmp(args[i], ">") == 0)
                 {
                     if (redirect)

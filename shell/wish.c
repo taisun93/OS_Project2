dd3b384e139c80527b8ebc6d58ff6d737e957eaa
taisun93 <taisun93@gmail.com>
2023-02-20 19:50:01 -0600

    a

--- shell/wish.c
+++ shell/wish.c
@@ -1,378 +1,289 @@
+#include "libs/cvector.h"
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <ctype.h>
+#include <unistd.h>
 
-#define MAX_INPUT 512
-#define MAX_ARGS 900
-
-int cd(char *args[])
+/**
+ * @brief prints error message to stderr
+ */
+void print_error()
 {
-    if (args[1] == NULL)
-    {
-        // No arguments specified, print error message
-        fprintf(stderr, "An error has occurred\n");
-        return 1;
-    }
-    else if (args[2] != NULL)
-    {
-        // Too many arguments specified, print error message
-        fprintf(stderr, "An error has occurred\n");
-        return 1;
-    }
-    else
-    {
-        // Change working directory
-        if (chdir(args[1]) != 0)
-        {
-            fprintf(stderr, "cd: %s: No such file or directory\n", args[1]);
-            return 1;
-        }
-
-        return 0;
-    }
+ char error_message[] = "An error has occurred\n";
+ write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-int path(char *args[])
+/**
+ * @brief checks if the given line is a valid command in terms of directing output to a file.
+ * @param line The line of input from the user.
+ * @param filename The name of the file to direct the output to.
+ * If NULL, line contains multiple filenames, mutiple '>', or no filename.
+ * @return True if there is a '>' in the line, false otherwise.
+ */
+bool redirect_output(char *line, char **filename)
 {
-    if (args[1] == NULL)
-    {
-        // No arguments specified, wipe path
-        if (setenv("PATH", "", 1) == -1)
-        {
-            perror("setenv");
-            return 1;
-        }
-    }
-
-    else
-    {
-        // Build new path string
-        char *path_env = getenv("PATH");
-        char new_path[strlen(path_env) + 1];
-        strcpy(new_path, path_env);
-        int i = 1;
-        while (args[i] != NULL)
-        {
-            strcat(new_path, ":");
-            strcat(new_path, args[i]);
-            i++;
-        }
-
-        // Set new path
-        if (setenv("PATH", new_path, 1) == -1)
-        {
-            perror("setenv");
-            return 1;
-        }
-        else
-        {
-            // Echo the new path
-            // fprintf(stdout, "New PATH=%s\n", new_path);
-        }
-    }
-    return 0;
+ // Check if the line contains a '>'
+ if (strchr(line, '>') == NULL)
+  return false;
+ // from: string before the '>', to: string after the '>'
+ char *from = strtok(line, ">");
+ char *to = strtok(NULL, "\n");
+ // handle the case where there's nothing after '>'
+ if (to == NULL)
+  return true;
+ // handle the case where there's multiple '>'s
+ if (strchr(to, '>') != NULL)
+  return true;
+ int filenum = 0;
+ // remove trailing spaces
+ char *token = strtok(to, " \0");
+ // if the token is not blank, then it's a filename
+ if (token != NULL)
+ {
+  (*filename) = malloc(sizeof(char) * strlen(token));
+  strcpy(*filename, token);
+ }
+ // handle the case where there's multiple files
+ while (token != NULL)
+ {
+  filenum++;
+  token = strtok(NULL, " \0");
+ }
+ // if there's more than one file, then it's an error
+ if (filenum != 1)
+ {
+  free(*filename);
+  (*filename) = NULL;
+  return true;
+ }
+ return true;
 }
 
-char *strtrim(char *str)
+/**
+ * @brief parse command to generate array of command arguments & handle command program path & command name.
+ * @param cmd command to parse
+ * @param path inital array of paths to search for command
+ * @param cmdName command name
+ * @return array of command arguments
+ */
+char **parse_cmd(char *cmd, char **path, char *cmdName)
 {
-    char *end;
+ char **cmdArgs = NULL;
+ // replace newline char with null char
+ if (cmd[strlen(cmd) - 1] == '\n')
+  cmd[strlen(cmd) - 1] = 0;
+ // split cmd into tokens
+ char *tmp = strtok(cmd, " \0");
+ if (tmp == NULL)
+  return cmdArgs;
+ // first token is the command name
+ strcpy(cmdName, tmp);
+ // create path to command program
+ char *token = malloc(sizeof(char) * 1024);
+ size_t size = cvector_size(path);
+ for (int i = 0; i < size; i++)
+ {
+  strcpy(token, path[i]);
+  strcat(token, "/");
+  strcat(token, tmp);
+  // if command program exists in path -> break
+  if (access(token, X_OK) != -1)
+   break;
+ }
+ // create array of command arguments
+ while (token != NULL)
+ {
+  cvector_push_back(cmdArgs, token);
+  token = strtok(NULL, " \0");
+ }
+ free(token);
+ return cmdArgs;
+}
 
-    // Trim leading spaces
-    while (isspace((unsigned char)*str))
-        str++;
+/**
+ * @brief splits parallel command into individual commands
+ * @param cmd the command to split
+ * @param parallel true if command is parallel, false otherwise
+ * @return array of commands
+ */
+char **flatten_parallel_commands(char *cmd, bool *parallel)
+{
+ char **cmdLines = NULL;
+ // if && found in cmd, split it into separate commands
+ if (strchr(cmd, '&') != NULL)
+  (*parallel) = true;
+ // relplace newline with null terminator
+ if (cmd[strlen(cmd) - 1] == '\n')
+  cmd[strlen(cmd) - 1] = 0;
+ // split cmd into separate commands & store in cmdLines
+ char *token = strtok(cmd, "&");
+ while (token != NULL)
+ {
+  cvector_push_back(cmdLines, token);
+  token = strtok(NULL, "&");
+ }
+ free(token);
+ return cmdLines;
+}
 
-    if (*str == 0) // All spaces?
-        return str;
+/**
+ * @brief execute a command
+ * @param cmdArgs the command and its arguments
+ * @param redirect true: redirect output to file, false: redirect output to stdout
+ * @param fileName the file name to redirect output to
+ */
+void execute_cmd(char **cmdArgs, bool is_redirect, char *filename, bool parallel)
+{
+ // fork a child process for new command
+ int rc = fork();
+ if (rc < 0) // fork faild
+ {
+  fprintf(stderr, "fork failed\n");
+  exit(EXIT_FAILURE);
+ }
+ else if (rc == 0) // child process
+ {
+  // command arguments array should be null terminated
+  cvector_push_back(cmdArgs, NULL);
+  int fd = 0;
+  // handle output direction
+  if (is_redirect)
+  {
+   int fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+   dup2(fd, STDOUT_FILENO);
+  }
+  // execute command
+  int status_code = execv(cmdArgs[0], cmdArgs);
+  // if execv failed
+  if (status_code == -1)
+   print_error();
+  if (is_redirect)
+   close(fd);
+  exit(EXIT_SUCCESS);
+ }
+ else // parent process
+ {
+  if (parallel)
+  {
+   int status_code = 0;
+   wait(&status_code);
+  }
+ }
+}
 
-    // Trim trailing spaces
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
+/**
+ * @brief Checks if the command is valid and executes it
+ * @param cmdArgs The command to execute
+ */
+void changeDirectory(char **cmdArgs)
+{
+ size_t size = cvector_size(cmdArgs);
+ if (size == 2 && chdir(cmdArgs[1]) == 0)
+  return;
+ print_error();
+}
 
-    // Write new null terminator character
-    end[1] = '\0';
-
-    return str;
+/**
+ * @brief replace old paths with new paths
+ * @param cmdArgs the command line arguments(new paths)
+ * @param path array of old paths
+ */
+void addPath(char **cmdArgs, char **path)
+{
+ size_t new_path_num = cvector_size(cmdArgs) - 1;
+ size_t cur_path_num = cvector_size(path);
+ // remove all paths
+ while (cur_path_num != 0)
+ {
+  cvector_pop_back(path);
+  cur_path_num--;
+ }
+ // add new paths
+ for (size_t i = 0; i < new_path_num; i++)
+ {
+  char *tmp = malloc(sizeof(char) * 512);
+  strcpy(tmp, cmdArgs[i + 1]);
+  cvector_push_back(path, tmp);
+ }
+ // assert that number of paths is valid
+ assert(cvector_size(path) == new_path_num);
 }
 
 int main(int argc, char *argv[])
 {
-
-    int interactive = (argc == 1) ? 1 : 0;
-    FILE *input_file = NULL;
-    setenv("PATH", "/bin", 1);
-
-    if (!interactive)
-    {
-        if (argc != 2)
-        {
-            fprintf(stderr, "An error has occurred\n");
-            exit(EXIT_FAILURE);
-        }
-
-        input_file = fopen(argv[1], "r");
-        if (input_file == NULL)
-        {
-            fprintf(stderr, "An error has occurred\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // char *path[100] = {"/bin", "/usr/bin", NULL};
-
-    char *input = NULL;
-    size_t input_len = 0;
-
-    while (1)
-    {
-        // Gets next command
-        if (interactive)
-        {
-            printf("wish> ");
-            fflush(stdout);
-            if (getline(&input, &input_len, stdin) == -1)
-            {
-                break;
-            }
-        }
-        else
-        {
-            // fprintf(stderr, "reading in \n");
-            if (getline(&input, &input_len, input_file) == -1)
-            {
-                break;
-            }
-        }
-
-        // break into seperate commands
-
-        // tokenization
-
-        while (*input == '\t' || *input == ' ' || *input == '\n')
-        {
-            input++;
-        }
-
-        // fprintf(stderr, "getting line\n");
-        char *args[MAX_INPUT];
-        int num_args = 0;
-
-        char *token = strtok(input, " ");
-
-        if (token == NULL)
-        {
-            continue;
-        }
-
-        while (token != NULL && num_args < MAX_INPUT)
-        {
-
-            if (strcmp(token, ">") != 0 && strstr(token, ">") != NULL)
-            {
-                char *arg1, *arg2;
-                arg1 = strsep(&token, ">");
-                arg2 = strsep(&token, ">");
-                args[num_args++] = arg1;
-                args[num_args++] = ">";
-                args[num_args++] = arg2;
-            }
-            //for the test case where this shit is sandwiched together
-            if (strcmp(token, "&") != 0 && strstr(token, "&") != NULL)
-            {
-                char *arg1, *arg2;
-                arg1 = strsep(&token, "&");
-                arg2 = strsep(&token, "&");
-                args[num_args++] = arg1;
-                args[num_args++] = "&";
-                args[num_args++] = arg2;
-            }
-            else
-            {
-                args[num_args++] = strtrim(token);
-            }
-            token = strtok(NULL, " \n");
-        }
-
-        args[num_args] = NULL; // Set last argument to NULL
-
-        // for (int i = 0; i < num_args; i++)
-        // {
-        //     printf(" args here %d, %s\n", i, args[i]);
-        // }
-
-        // crazy if else begins, execution begins
-        if (strcmp(args[0], "exit") == 0)
-        {
-
-            if (num_args == 1) // check for no extra args
-            {
-                if (interactive)
-                {
-                    break;
-                }
-                exit(0);
-            }
-            else
-            {
-                fprintf(stderr, "An error has occurred\n");
-            }
-        }
-        else if (strcmp(args[0], "cd") == 0)
-        {
-            cd(args);
-        }
-        else if (strcmp(args[0], "path") == 0)
-        {
-            path(args);
-        }
-        // execute shit
-        else
-        {
-            int redirect = 0;
-            // Child process
-            char *new_args[MAX_ARGS];
-            int i, fd;
-
-            for (i = 0; args[i] != NULL; i++)
-            {
-                // fprintf(stdout, "start start %s \n", args[i]);
-                if (strcmp(args[i], ">") == 0)
-                {
-                    if (redirect)
-                    {
-                        redirect = 2;
-                    }
-                    else
-                    {
-                        redirect = 1;
-                        if (args[i + 1] != NULL)
-                        {
-                            fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-                        }
-                    }
-                }
-                new_args[i] = args[i];
-                // fprintf(stdout, "blah %s \n", args[i]);
-            }
-
-            // can't chain redirects
-            if (redirect > 1)
-            {
-                fprintf(stderr, "An error has occurred\n");
-                continue;
-            }
-
-            new_args[i + 1] = NULL;
-
-            char *path_env = getenv("PATH");
-            char *path = strdup(path_env);
-            char *dir = strtok(path, ":");
-            char full_path[90];
-            int saved_stdout = dup(1);
-
-            while (dir != NULL)
-            {
-                sprintf(full_path, "%s/%s", dir, args[0]);
-                if (access(full_path, X_OK) == 0)
-                {
-                    break;
-                }
-                dir = strtok(NULL, ":");
-            }
-            free(path);
-
-            // can't find command
-            if (dir == NULL)
-            {
-                fprintf(stderr, "An error has occurred\n");
-                continue;
-            }
-
-            // can't end on >
-            if (strcmp(args[i - 1], ">") == 0)
-            {
-                fprintf(stderr, "An error has occurred\n");
-                continue;
-            }
-
-            // penultimate in a redirect needs to be a >
-            if (redirect && strcmp(args[i - 2], ">") != 0)
-            {
-                fprintf(stderr, "An error has occurred\n");
-                continue;
-            }
-            // redirect here
-            if (redirect)
-            {
-                dup2(fd, STDOUT_FILENO);
-                dup2(fd, STDERR_FILENO);
-                new_args[i - 2] = NULL;
-                new_args[i - 1] = NULL;
-            }
-
-            int num_commands = 1;
-            for (int j = 0; j < i; j++)
-            {
-                if (strcmp(new_args[j], "&") == 0)
-                {
-                    new_args[j] = NULL;
-                    num_commands++;
-                }
-            }
-
-            if (strcmp(args[0], "&") == 0)
-            {
-                continue;
-            }
-
-            // Run commands in parallel
-
-            int current_command = 0;
-            for (int j = 0; j <= i; j++)
-            {
-                if (new_args[j] == NULL || strcmp(new_args[j], "&") == 0)
-                {
-                    new_args[j] = NULL;
-                    pid_t pid = fork();
-                    if (pid == 0)
-                    {
-                        if (execv(full_path, &new_args[current_command]) == -1)
-                        {
-                            fprintf(stderr, "An error has occurred\n");
-                            exit(EXIT_FAILURE);
-                        }
-                    }
-                    else if (pid < 0)
-                    {
-                        fprintf(stderr, "An error has occurred\n");
-
-                        exit(EXIT_FAILURE);
-                    }
-                    else
-                    {
-
-                        wait(NULL);
-                    }
-
-                    if (redirect)
-                    {
-                        close(fd);
-                        // dup2(saved_stdout, 1);
-                        dup2(saved_stdout, STDOUT_FILENO);
-                        dup2(saved_stdout, STDERR_FILENO);
-                    }
-                }
-            }
-        }
-    }
-
-    free(input);
-
-    return 0;
+ char **path = NULL;
+ bool interactive = false;
+ FILE *input = NULL;
+ char buffer[1024];
+ char cmdName[1024];
+ // initialize path
+ cvector_push_back(path, "/bin");
+ cvector_push_back(path, "/usr/bin");
+ // handle different modes
+ if (argc == 1) // interactive mode
+ {
+  interactive = true;
+  input = stdin;
+ }
+ else if (argc == 2) // batch mode
+ {
+  input = fopen(argv[1], "r");
+  if (input == NULL)
+  {
+   print_error();
+   exit(EXIT_FAILURE);
+  }
+ }
+ else
+ {
+  print_error();
+  exit(EXIT_FAILURE);
+ }
+ // print prompt
+ if (interactive)
+  printf("wish> ");
+ while (fgets(buffer, sizeof(buffer), input) != NULL)
+ {
+  // print prompt
+  if (interactive)
+   printf("wish> ");
+  // split buffer into multiple commands
+  bool parallel = false;
+  char **cmdlines = flatten_parallel_commands(buffer, &parallel);
+  size_t ncmd = cvector_size(cmdlines);
+  // for every command in buffer
+  for (size_t i = 0; i < ncmd; i++)
+  {
+   char *filename = NULL;
+   // get output stream (file or stdout)
+   bool redirect = redirect_output(cmdlines[i], &filename);
+   // command is not valid
+   if ((redirect == true) && (filename == NULL))
+   {
+    print_error();
+    continue;
+   }
+   // parse command
+   char **cmdArgs = parse_cmd(cmdlines[i], path, cmdName);
+   if (cmdArgs == NULL)
+    continue;
+   // execute command
+   if (strcmp(cmdName, "exit") == 0)
+    exit(EXIT_SUCCESS);
+   else if (strcmp(cmdName, "cd") == 0)
+    changeDirectory(cmdArgs);
+   else if (strcmp(cmdName, "path") == 0)
+    addPath(cmdArgs, path);
+   else
+    execute_cmd(cmdArgs, redirect, filename, parallel);
+   cvector_free(cmdArgs);
+  }
+  for (size_t i = 0; i < ncmd; i++)
+   wait(NULL);
+  cvector_free(cmdlines);
+ }
+ exit(EXIT_SUCCESS);
 }
